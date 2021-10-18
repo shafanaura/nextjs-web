a6897997f9e8f9b13e532a34bc9e8719266100f8
shafanaura <shafanaura48@gmail.com>
2021-10-18 10:28:30 +0700

    fix

--- pages/theme.js
+++ pages/theme.js
@@ -1,30 +1,28 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 // Create a theme instance.
-const theme = createMuiTheme({
-	palette: {
-		basic: {
-			main: "#313131",
-		},
-		primary: {
-			main: "#FCC932",
-		},
-		secondary: {
-			main: "#2DABFF",
-		},
-		error: {
-			main: "#E74C3C",
-		},
-		success: {
-			main: "#00BA42",
-		},
-		background: {
-			default: "#fff",
-		},
-	},
-	typography: {
-		fontFamily: "Work Sans, sans-serif",
-	},
+export const theme = createMuiTheme({
+  palette: {
+    basic: {
+      main: "#313131",
+    },
+    primary: {
+      main: "#FCC932",
+    },
+    secondary: {
+      main: "#2DABFF",
+    },
+    error: {
+      main: "#E74C3C",
+    },
+    success: {
+      main: "#00BA42",
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: "Work Sans, sans-serif",
+  },
 });
-
-export default theme;
